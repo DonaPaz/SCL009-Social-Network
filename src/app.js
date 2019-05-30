60b7c18333052ab8b64659109d7df4c5a4f79941
donapaz <danielapaz.pl@gmail.com>
2019-05-30 18:11:27 -0400

    showing firebase username

--- src/app.js
+++ src/app.js
@@ -1,49 +1,90 @@
 (function() {
 
-      
      
     // Obtener elementos
+    const txtUser = document.getElementById('txtUser');
     const txtEmail = document.getElementById('txtEmail');
     const txtPassword = document.getElementById('txtPassword');
+    
     const btnLogin = document.getElementById('btnLogin');
     const btnSignUp = document.getElementById('btnSignUp');
     const btnLogout = document.getElementById('btnLogout');
   
-    // Añadir Evento login
+    // LOGIN EVENT
     btnLogin.addEventListener('click', e => {
       //Obtener email y pass
       const email = txtEmail.value;
       const pass = txtPassword.value;
       const auth = firebase.auth();
-      // Sign in
+      
+      // LOGIN // PASS AND EMAIL ADDRESS 
       const promise = auth.signInWithEmailAndPassword(email, pass);
       promise.catch(e => console.log(e.message));   
     });
   
-    // Añadir evento signup
+    // REGISTRER EVENT
     btnSignUp.addEventListener('click', e => {
       // Obtener email y pass
       // TODO: comprobar que el email sea real
+      const userName = txtUser.value;
       const email = txtEmail.value;
       const pass = txtPassword.value;
       const auth = firebase.auth();
-      // Sign in
-      const promise = auth.createUserWithEmailAndPassword(email, pass);
-      promise.catch(e => console.log(e.message));
+      
+      // CREATE A NEW ACCOUNT // PASS AND EMAIL ADDRESS 
+      const promise = auth.createUserWithEmailAndPassword(email, pass)
+      .then(function(){
+        verify()
+
+        var user = firebase.auth().currentUser;
+
+        user.updateProfile({
+        displayName: userName
+        }).then(function() {
+        // Update successful.
+        }).catch(function(error) {
+        // An error happened.
+        });
+      
+      })
+     promise.catch(e => console.log(e.message));
+     alert("Ingresa un mail válido");
     });
   
+
     btnLogout.addEventListener('click', e => {
       firebase.auth().signOut();
     });
   
+
     // Añadir un listener en tiempo real
      firebase.auth().onAuthStateChanged( firebaseUser => {
       if(firebaseUser) {
         console.log(firebaseUser);
-        btnLogout.classList.remove('hide');
+        btnLogout.style.display="inline";
+        
+        document.getElementById("show-msg").innerHTML = firebaseUser.displayName;
+
       } else {
         console.log('no logueado');
-        btnLogout.classList.add('hide');
+        btnLogout.style.display="none";
+        document.getElementById("show-msg").innerHTML = " "
       }    
     });
+
+
+    //SEND A USER VERIFICATION EMAIL
+    function verify (){
+    let user = firebase.auth().currentUser;
+    user.sendEmailVerification().then(function() {
+      console.log("enviando correo");
+      // Email sent.
+    }).catch(function(error) {
+      console.log("error")
+      // An error happened.
+    });
+    
+
+  }
+
   } ());
