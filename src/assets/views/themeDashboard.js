68e06491dff577fff828afbf1c58ae2350558533
alelulu <ale.silvasuazo@gmail.com>
2019-06-11 13:37:53 -0400

    saving post in firebase and getting it

--- src/assets/views/themeDashboard.js
+++ src/assets/views/themeDashboard.js
@@ -1,28 +1,45 @@
 import { themeProfile } from './themeProfile.js';
 import { logOut } from '../js/logout.js';
 import { themePreferences } from './themePreferences.js';
-//
+import { savePost, getPost } from '../js/auth.js'
 
-
+//Creating daashboard template. Here the user should be able to write a post and see it
 export const themeDashboard = () => {
- 
-  //observer() 
-  //Acá se muestra input nombre, contraseña, correo. botón enviar y botón ingresar con google
   document.body.style.background="#EA77A6";
-  document.getElementById('navbar').innerHTML=`  <img src="./img/logo.png"  id="logo" alt="logo">
-  <div class="dropdown">
-  <button id="btn-drop" class="dropbtn">Perfil</button>
-  <div id="myDropdown" class="dropdown-content">
-    <a id="btn-logout">Cerrar sesión</a>
-    <a id="btn-profile">Perfil</a>
-    <a id="btn-preference">favoritos</a>
-  </div>
-</div>              
+  document.getElementById('navbar').innerHTML = `<img src="./img/logo.png"  id="logo" alt="logo">
+                                                <div class="dropdown">
+                                                  <button id="btn-drop" class="dropbtn">Perfil</button>
+                                                  <div id="myDropdown" class="dropdown-content">
+                                                    <a id="btn-logout">Cerrar sesión</a>
+                                                    <a id="btn-profile">Perfil</a>
+                                                    <a id="btn-preference">favoritos</a>
+                                                  </div>
+                                                </div>              
   `
   document.getElementById('content').innerHTML = `<p>Tu muro</p>
-                                              <button id="btn-profile">Ir al perfil</button>`
+                                                  <div id="post-header">
+                                                    <p>Nombre user</p>
+                                                  </div>
+                                                  <div id=post-content>
+                                                    <textarea id="user-txt" rows="4" cols="40" placeholder="Escribe aquí"></textarea>
+                                                  </div>
+                                                  <button id="send-btn">Enviar</button>
+                                                  <h6>Todos los posts</h6>
+                                                  <div id="posts"></div>
+                                                  <button id="btn-profile">Ir al perfil</button>`
 
- 
+  document.getElementById('send-btn').addEventListener('click', () => {
+    savePost();
+    getPost();
+    document.getElementById('user-txt').textContent = ""
+    let posts = document.getElementById('posts');
+    let postContent = document.createElement('div')
+    let postTxt = document.createElement('p')
+    //postTxt.innerHTML = "'"+doc.data()+"'";
+    postContent.appendChild(postTxt);
+    posts.appendChild(postContent);
+  })
+
  //navbar
  // AL BAJAR 80 PX SE ADAPTA EL NAVBAR
  window.onscroll = function() {scrollFunction()};
